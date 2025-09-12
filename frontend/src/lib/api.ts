bdfbfe272b9cd76febaf0083c874aec642f8e66d
CODSensei <krishna2342005@gmail.com>
2025-09-12 23:39:34 +0530

    feat: hono RPC

---
+++ frontend/src/lib/api.ts
@@ -0,0 +1,6 @@
+import { type apiRoutes } from "@server/app";
+import { hc } from "hono/client";
+
+const client = hc<apiRoutes>("/");
+
+export const api = client.api;
--- server/app.ts
+++ server/app.ts
@@ -2,17 +2,17 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { expensesRoute } from "./routes/expenses";
+import { serveStatic } from "hono/bun";
 
 // this is out main app
 const app = new Hono();
 app.use("*", logger());
 
-// a test route to see if server is working
-app.get("/test", (c) => {
-  return c.json({ message: "test" });
-});
-
 // sub-app will be mounted on this route
-app.route("/api/expenses", expensesRoute);
+const apiRoutes = app.basePath("/api").route("/expenses", expensesRoute);
+
+app.get("*", serveStatic({ root: "./frontend/dist" }));
+app.get("*", serveStatic({ path: "./frontend/dist/index.html" }));
 
 export default app;
+export type apiRoutes = typeof apiRoutes;
